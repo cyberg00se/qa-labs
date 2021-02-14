4d771ecb007fee7682522268e3aab76b34c11d82
cyberg00se <zhuk.bohdana@gmail.com>
2021-02-14 16:46:45 +0200

    Added exceptionIsCritical test

--- lab1/src/ExceptionManagerTest.java
+++ lab1/src/ExceptionManagerTest.java
@@ -1,5 +1,26 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExceptionManagerTest {
+    @ParameterizedTest
+    @ValueSource(classes = {NullPointerException.class, ArrayIndexOutOfBoundsException.class, FileNotFoundException.class})
+    public void exceptionIsCritical(Class exceptionClass)
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        //arrange
+        ExceptionManager manager = new ExceptionManager();
+        Exception inputException = (Exception)exceptionClass.getDeclaredConstructor().newInstance();
 
+        boolean expected = true;
+
+        //act
+        boolean actual = manager.isCritical(inputException);
+
+        //assert
+        assertEquals(expected, actual);
+    }
 }
