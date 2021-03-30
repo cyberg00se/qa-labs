f80452dfd00dec84e9c642d7e5174169e57d4a59
cyberg00se <zhuk.bohdana@gmail.com>
2021-03-30 12:50:47 +0300

    Init lab3

---
+++ qa_lab3/src/test/java/TodosPage.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class TodosPage {
+    public WebDriver driver;
+    public TodosPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "/html/body/ng-view/section/header/form/input")
+    private WebElement newTodoField;
+    private int todosCount;
+
+    private List<WebElement> todoNamesList;
+    private List<WebElement> todoChecksList;
+
+    public int getTodosCount() {
+        return todosCount;
+    }
+
+    public void inputNewTodo(String todo) {
+        newTodoField.sendKeys(todo + Keys.ENTER);
+        todosCount++;
+    }
+
+   /* public void checkTodo(int index) {
+        todoChecksList = driver.findElements(By.cssSelector("ul.todo-list > li"));
+        todoChecksList.get(index).click();
+    }*/
+
+    public String getTodoName(int index) {
+        todoNamesList = driver.findElements(By.cssSelector("label.ng-binding"));
+        return todoNamesList.get(index).getText();
+    }
+
+    /*public boolean getTodoIsChecked(int index){
+        todoChecksList = driver.findElements(By.cssSelector("ul.todo-list > li"));
+        return todoNamesList.get(index).getAttribute("class").contains("completed");
+    }*/
+}
---
+++ qa_lab3/src/test/java/TodosTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodosTest {
+    public static TodosPage todosPage;
+    public static WebDriver driver;
+
+    @BeforeAll
+    public static void setup() {
+        System.setProperty("webdriver.gecko.driver", "geckodriver");
+        driver = new FirefoxDriver();
+        todosPage = new TodosPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperty("todospage"));
+    }
+
+    @Test
+    public void makeNewTodo() {
+        todosPage.inputNewTodo("my task 1");
+        assertEquals("my task 1", todosPage.getTodoName(todosPage.getTodosCount()-1));
+    }
+
+    /*@Test
+    public void checkNewTodoTest(){
+        todosPage.inputNewTodo("my task 2");
+        todosPage.checkTodo(todosPage.getTodosCount()-1);
+        assertEquals(true, todosPage.getTodoIsChecked(todosPage.getTodosCount()-1));
+    }*/
+
+    @AfterAll
+    public static void tearDown() {
+        //driver.quit();
+    }
+}
